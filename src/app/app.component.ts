8b4897b32820505b923a332569ab2b7133a809eb
KBeltz <beltz08@me.com>
2021-06-24 14:32:17 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserPermissionService } from './core/user-permission.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   isAdmin: boolean = true;
+  constructor(private userPermissionService: UserPermissionService){}
+  ngOnInit(){
+    this.isAdmin = this.userPermissionService.isAdmin();
+  }
 }
--- src/app/core/user-permission.service.ts
+++ src/app/core/user-permission.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class UserPermissionService {
-
+  private isAdminValue: boolean = false;
   constructor() { }
+
+  isAdmin(): boolean {
+    if(!this.isAdminValue) {
+      this.isAdminValue = Math.random() % 2 === 0;
+    }
+    return this.isAdminValue;
+  }
 
 }
