16b4b4d4a582f728e9eb5bb31aed0f2583756d09
KBeltz <beltz08@me.com>
2021-06-24 15:09:42 -0500

    fix always admin

--- src/app/core/user-permission.service.ts
+++ src/app/core/user-permission.service.ts
@@ -2,16 +2,15 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class UserPermissionService {
-  private isAdminValue: boolean = false;
+  private isAdminValue: number;
   constructor() { }
 
   isAdmin(): boolean {
     if(!this.isAdminValue) {
-      const num = Math.floor(Math.random() * 100) + 1;
-      console.log('Random number', num);
-      this.isAdminValue = num % 2 === 0;
+      this.isAdminValue = Math.floor(Math.random() * 100) + 1;
+      console.log('Random number', this.isAdminValue);
     }
-    return this.isAdminValue;
+    return this.isAdminValue % 2 === 0;
   }
 
 }
