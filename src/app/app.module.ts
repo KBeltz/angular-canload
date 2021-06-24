b484c9262285223cdb04ee50720e7f0c1862227c
KBeltz <beltz08@me.com>
2021-06-24 14:18:35 -0500

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
-
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule, CoreModule, SharedModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ],
-  providers: []
 })
 export class AppModule { }
